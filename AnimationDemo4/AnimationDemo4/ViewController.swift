bf0fef7302d206af3e3f6f7f829eea738bdc78e0
Geeven <598800360@qq.com>
2016-03-16 17:02:47 +0800

    init animation

---
+++ AnimationDemo4/AnimationDemo4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationDemo4
+//
+//  Created by Geeven on 16/3/16.
+//  Copyright © 2016年 Geeven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
